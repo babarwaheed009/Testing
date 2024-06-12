d1d452b9f860b4adf0744e1533f789470458e7fd
Babar Waheed <babarwaheed009@gmail.com>
2024-06-13 02:38:17 +0500

    lets start

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          Welcome to the React JS.
+          Welcome to the React JS. Let's Start
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
